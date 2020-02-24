9809b4a591592f7352a33d2443ee18a6c7acf031
Daichi Kimura <daichi703n@gmail.com>
2020-02-24 17:58:33 +0900

    Bump chart to 0.1.1

--- commands/install.sh
+++ commands/install.sh
@@ -1,1 +1,1 @@
-helm install --name elastalert --namespace elastic elastalert-0.1.0.tgz
+helm install --name elastalert --namespace elastic elastalert-0.1.1.tgz
--- commands/upgrade.sh
+++ commands/upgrade.sh
@@ -1,1 +1,1 @@
-helm upgrade --recreate-pods -f vars.yml --install elastalert --namespace elastic elastalert-*.tgz
+helm upgrade --recreate-pods -f vars.yml --install elastalert --namespace elastic elastalert-0.1.1.tgz
